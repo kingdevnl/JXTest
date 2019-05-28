c71b387677194ce7a58819a09845dbad6ef67c6d
KingdevNL <jasper@kingdev.nl>
2019-05-28 11:48:20 +0200

    Add message

--- src/main/java/com/example/JXTest/DemoApplication.java
+++ src/main/java/com/example/JXTest/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Loading up!");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
